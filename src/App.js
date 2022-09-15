524a2047998f47707880b6af82385f2adc4127ea
Zachary Turney <99896219+turneyz@users.noreply.github.com>
2022-09-14 20:58:25 -0700

    Changed general text on screen to reflect application

--- src/App.js
+++ src/App.js
@@ -9,16 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my Basic Counter page, this is my first react project, let's see what's next!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Counter />
       </header>
     </div>
   );
